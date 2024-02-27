bff5a20c990b0a08f40451ef3ca440fecc3f6bf4
64410047 <153884268+64410047@users.noreply.github.com>
2024-02-27 17:14:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/courses/data/DataSource.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.courses.data
+
+import com.example.courses.R
+import com.example.courses.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.image1, 58, R.drawable.image1),
+        Topic(R.string.image2, 121, R.drawable.image2),
+        Topic(R.string.image3, 78, R.drawable.image3),
+        Topic(R.string.image4, 118, R.drawable.image4),
+        Topic(R.string.image5, 423, R.drawable.image5),
+        Topic(R.string.image6, 92, R.drawable.image6),
+        Topic(R.string.image7, 165, R.drawable.image7),
+        Topic(R.string.image8, 164, R.drawable.image8),
+        Topic(R.string.image9, 326, R.drawable.image9),
+        Topic(R.string.image10, 305, R.drawable.image10),
+        Topic(R.string.ra1, 305, R.drawable.ra1),
+        Topic(R.string.ra2, 305, R.drawable.ra2),
+        Topic(R.string.ra3, 305, R.drawable.ra3),
+        Topic(R.string.ra4, 305, R.drawable.ra4),
+        Topic(R.string.ra5, 305, R.drawable.ra5),
+        Topic(R.string.ra6, 305, R.drawable.ra6),
+        Topic(R.string.ra7, 305, R.drawable.ra7),
+        Topic(R.string.ra8, 305, R.drawable.ra8),
+        Topic(R.string.ra9, 305, R.drawable.ra9),
+        Topic(R.string.ra10, 305, R.drawable.ra10),
+        Topic(R.string.ra11, 305, R.drawable.ra11),
+        Topic(R.string.ra12, 305, R.drawable.ra12),
+        Topic(R.string.ra13, 305, R.drawable.ra13),
+        Topic(R.string.ra14, 305, R.drawable.ra14),
+        Topic(R.string.ra15, 305, R.drawable.ra15),
+    )
+}
